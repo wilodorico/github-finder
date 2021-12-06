8d5c4efb003dc58db5e68ce38554b724bc7b65c7
wilfried <wilfried.odorico@gmail.com>
2021-12-06 10:29:03 +0100

    first-commit

---
+++ src/components/users/Search.js
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+import AlertContext from '../../context/alert/AlertContext'
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertcontext = useContext(AlertContext);
+
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      alertcontext.setAlert("Please enter something", "light");
+    } else {
+      githubContext.searchUsers(text);
+      setText("");
+    }
+  };
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/components/users/User.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useContext } from "react";
+import Spinner from "../layout/Spinner";
+import Repos from "../repos/Repos";
+import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/GithubContext";
+
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, getUserRepos, repos, loading, user } = githubContext;
+  
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
+
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+    company,
+  } = user;
+
+  if (loading) return <Spinner />;
+  return (
+    <>
+      <Link to="/" className="btn btn-light">
+        Back to search
+      </Link>
+      Hireable:{" "}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            style={{ width: "150px" }}
+            alt="profil"
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit Github Profil
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <>
+                  <strong>Username: </strong> {login}
+                </>
+              )}
+            </li>
+            <li>
+              {company && (
+                <>
+                  <strong>Company: </strong> {company}
+                </>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <>
+                  <strong>Website: </strong> {blog}
+                </>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos} />
+    </>
+  );
+};
+
+export default User;
